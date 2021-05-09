a79cb912c754eb3bc2f1a0adc7b8a279709d30ff
Robert Briza <rbriza@retia.cz>
2021-05-09 08:31:15 +0200

    Zaklad projektu

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="grid md:grid-cols-2 gap-4">
+        <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Případy</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Nové:</span>
+                {{stats.NewConfirmed}}
+            </div><div class="text-2xl mb-4">
+                <span class="font-bold">Celkem:</span>
+                {{stats.TotalConfirmed}}
+            </div>
+        </div>
+        <div class="shadow-md bg-red-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Úmrtí</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Nové:</span>
+                {{stats.NewDeaths}}
+            </div><div class="text-2xl mb-4">
+                <span class="font-bold">Celkem:</span>
+                {{stats.TotalDeaths}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'DataBoxes',
+    props: ['stats']
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
+    </div>
+    <p>API od <a class="text-blue-300" href="https://covid19api.com" target="_blank">https://covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
